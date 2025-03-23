d72acc32bf9655631e4ce2bde54ccf730df95e9c
cpesar <cpesar1@gmail.com>
2025-03-23 00:49:19 -0600

    added resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,36 @@
-const ResumePage = () => {
-  return <>Resume Page</>;
+import React from "react";
+
+const ResumePage: React.FC = () => {
+  const docId = import.meta.env.VITE_RESUME_DOC_ID;
+
+  const embedUrl = `https://docs.google.com/document/d/${docId}/preview`;
+
+  return (
+    <div
+      className="resume-container"
+      style={{
+        width: "100%",
+        height: "100vh",
+        padding: "0",
+        margin: "0",
+        overflow: "hidden",
+      }}
+    >
+      <iframe
+        src={embedUrl}
+        title="Resume"
+        width="100%"
+        height="100%"
+        style={{
+          border: "none",
+          overflow: "hidden",
+          backgroundColor: "#fff",
+        }}
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
+  );
 };
 
 export default ResumePage;
